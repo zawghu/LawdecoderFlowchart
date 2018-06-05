e16c65689099b95537a26bf63987f7e6a8b8f182
zawghu <mselvag1@swarthmore.edu>
2018-06-05 09:42:08 -0400

    added php generation of bubbles using xml file

--- flowchart.php
+++ flowchart.php
@@ -1,17 +1,9 @@
+
 <!DOCTYPE html>
 <html>
 <head>
   <link rel="stylesheet" href="flowchart.css">
 </head>
-
-<!-- potentially use column technique*
-
-Idea: use php to take in xml document that for each bubble contains
-1. name/title
-2. text to display when clicked
-3. type of div (one type would be blank to allow for spacing)
-4. whether div is the end of a column
--->
 
 <body>
   <div>
@@ -20,73 +12,42 @@ Idea: use php to take in xml document that for each bubble contains
       <p>^ↀᴥↀ^<br>Meow. I'm a huge cat with a lot of information.</p>
     </div>
     <div class="flowchart">
-      <div class="column">
-        <div id="test1" class="bubble preprocess">
-          <p>Test 1 Yeah this is test 1 wowie</p>
-        </div>
-
-        <div id="test2" class="bubble offerprocess">
-          <p>Test 2</p>
-        </div>
-
-        <div id="test3" class="bubble postprocess">
-          <p>Test 3</p>
-        </div>
-
-        <div class="bubble postprocess">
-          <p>Test 3</p>
-        </div>
-        <div class="bubble empty">
-          <p>Test 3</p>
-        </div>
-      </div>
-      <div class="column">
-        <div id="test4" class="bubble preprocess">
-          <p>Test 1 Yeah this is test 1 wowie</p>
-        </div>
-        <div class="bubble offerprocess">
-          <p>Test 2</p>
-        </div>
-        <div class="bubble empty">
-          <p>Test 3</p>
-        </div>
-        <div class="bubble postprocess">
-          <p>Test 3</p>
-        </div>
-        <div class="bubble postprocess">
-          <p>Test 3</p>
-        </div>
-        </div>
-      </div>
+      <?php
+        $columns = simplexml_load_file('test.xml');
+        foreach ($columns->column as $column) {
+          print "<div class=\"column\">";
+          foreach ($column->bubble as $bubble) {
+            print "<div id=\"{$bubble->name}\" class=\"bubble {$bubble->type}\"><p>{$bubble->name}</p></div>";
+          }
+          print "</div>";
+        }
+      ?>
     </div>
   </div>
 </body>
 <script>
-  //have php automatically generate the number of times to call these functions
-  document.getElementById("test1").addEventListener("click", function(){
-    changeText("Test 1", " Woof. I'm a huge dog with no information", "test1");
-  });
-  document.getElementById("test4").addEventListener("click", function(){
-    changeText("Test 4", " yeah", "test4");
-  });
-
-  document.getElementById("test2").addEventListener("click", function(){
-    changeText("Test 2", " Glub. I'm a small fish with some information", "test2");
-  });
-
-  document.getElementById("test3").addEventListener("click", function(){
-    changeText("Test 3", " Ribbit. I'm a huge frog with enough information", "test3");
-  });
+  <?php
+  $columns = simplexml_load_file('test.xml');
+  foreach ($columns->column as $column) {
+    foreach ($column->bubble as $bubble) {
+      if ($bubble->type != "empty" && $bubble->type != "default") {
+        print "document.getElementById(\"{$bubble->name}\").addEventListener(\"click\", function(){
+          changeText(\"{$bubble->name}\", \"{$bubble->text}\");
+        });";
+      }
+    }
+  }
+  ?>
 
   var previousBubble;
-  function changeText(title, description, id) {
+  function changeText(title, description) {
     document.getElementById("info").innerHTML = "<h1>" + title  + "</h1><p>" + description + "</p>";
 
-    document.getElementById(id).classList.add('selected');
+    document.getElementById(title).classList.add('selected');
     if (previousBubble) {
       document.getElementById(previousBubble).classList.remove('selected');
     }
-    previousBubble = id;
+    previousBubble = title;
   }
 </script>
 
