e1d7471adae68108589e666a26aaa47d303b1f22
zawghu <mselvag1@swarthmore.edu>
2018-06-18 11:57:29 -0400

    used jquery instead of php to add interactivity and allowed for customizable default text

--- flowchart.php
+++ flowchart.php
@@ -6,143 +6,130 @@
 </head>
 <body>
   <div id="outer">
-    <div id="info" class="information">
-      <h1>Housing Flowchart</h1>
-      <p>These are the steps necessary for buying a house. Click on any step for additional details!</p>
-    </div>
-    <div class="flowchart">
-      <?php //This php takes in an xml file, and using its information, generates a flowchart
-        $columns = simplexml_load_file('selling.xml');
-        foreach ($columns->column as $column) {
-          //if a column is titled arrow, it generates a thinner column for display of arrows
-          if ($column->title == "arrow") {
-            print "<div class=\"column arrow-column\">";
-            //this iterates through each arrow in column
-            foreach ($column->arr as $arr) {
-              //if arr named empty, uses its height for spacing
-              if ($arr->name == "empty") {
-                print "<div style=\"height:{$arr->height}px;\" class=\"empty\"></div>";
-              }
-              //this prints out the proper kind of arrow depending on its name
-              switch($arr->name) {
-                case "left":
-                  print "<div class=\"horizontal-arrow\">&larr;</div>";
-                  break;
-                case "right":
-                  print "<div class=\"horizontal-arrow\">&rarr;</div>";
-                  break;
-                case "NW":
-                  print "<div class=\"horizontal-arrow\">&#8598;</div>";
-                  break;
-                case "NE":
-                  print "<div class=\"horizontal-arrow\">&#8599;</div>";
-                  break;
-                case "SE":
-                  print "<div class=\"horizontal-arrow\">&#8600;</div>";
-                  break;
-                case "SW":
-                  print "<div class=\"horizontal-arrow\">&#8601;</div>";
-                  break;
-              }
-            }
+    <?php //This php takes in an xml file, and using its information, generates a flowchart
+    $columns = simplexml_load_file('selling.xml');
+    foreach ($columns->column as $column) {
+      if ($column->title == "defaulttext") {
+        print "<div id=\"info\" class=\"{$column->bubble->type}\"><h1>{$column->bubble->name}</h1><p>{$column->bubble->text}</p></div>";
+        print "<div class=\"flowchart\">";
+      }
+      //if a column is titled arrow, it generates a thinner column for display of arrows
+      else if ($column->title == "arrow") {
+        print "<div class=\"column arrow-column\">";
+        //this iterates through each arrow in column
+        foreach ($column->arr as $arr) {
+          //if arr named empty, uses its height for spacing
+          if ($arr->name == "empty") {
+            print "<div style=\"height:{$arr->height}px;\" class=\"empty\"></div>";
           }
-          else {
-            print "<div class=\"column\">";
-            foreach ($column->bubble as $bubble) {
-              if($bubble->type == "empty") {
-                print "<div style=\"height:{$bubble->height}px;\" class=\"empty\"></div>";
-              } else {
-                if($bubble->arrow) {
-                  print "<div id=\"{$bubble->name}\" class=\"bubble arrow-bubble {$bubble->type}\"><p>{$bubble->name}</p></div>";
-                } else {
-                  print "<div id=\"{$bubble->name}\" class=\"bubble {$bubble->type}\"><p>{$bubble->name}</p></div>";
-                }
-                if ($bubble->type != "default") {
-                  print "<div class=\"contents\">{$bubble->text}</div>";
-                }
-                switch($bubble->arrow) {
-                  case "up":
-                    print "<div class=\"vertical-arrow\"><p>&uarr;</p></div>";
-                    break;
-                  case "down":
-                    print "<div class=\"vertical-arrow\">&darr;</div>";
-                    break;
-                  case "updown":
-                    print "<div class=\"vertical-arrow\">&#8597;</div>";
-                    break;
-                }
-              }
-            }
+          //this prints out the proper kind of arrow depending on its name
+          switch($arr->name) {
+            case "left":
+              print "<div class=\"horizontal-arrow\">&larr;</div>";
+              break;
+            case "right":
+              print "<div class=\"horizontal-arrow\">&rarr;</div>";
+              break;
+            case "NW":
+              print "<div class=\"horizontal-arrow\">&#8598;</div>";
+              break;
+            case "NE":
+              print "<div class=\"horizontal-arrow\">&#8599;</div>";
+              break;
+            case "SE":
+              print "<div class=\"horizontal-arrow\">&#8600;</div>";
+              break;
+            case "SW":
+              print "<div class=\"horizontal-arrow\">&#8601;</div>";
+              break;
           }
-          print "</div>";
         }
-      ?>
-    </div>
-    <div class="dropdown">
-      <?php
-        $priorType = "";
-        $first = true;
-        print "<div>";
-        $columns = simplexml_load_file('selling.xml');
-        foreach ($columns->column as $column) {
-          if ($column->title != "arrow") {
-            foreach ($column->bubble as $bubble) {
-              if($bubble->type != "empty") {
-                if($priorType != $bubble->type) {
-                  print "</div><div class=\"parent bubble {$bubble->type} column-title\"><p>{$column->title}</p></div>";
-                  print "<div class=\"child\">";
-                }
-                print "<div id=\"{$bubble->name}\" class=\"bubble parent {$bubble->type}\"><p>{$bubble->name}</p></div>";
-                if ($bubble->type != "default") {
-                  print "<div class=\"child contents\">{$bubble->text}</div>";
-                }
-                $priorType = (string)$bubble->type;
-              }
+        print "</div>";
+      }
+      else {
+        print "<div class=\"column\">";
+        foreach ($column->bubble as $bubble) {
+          if($bubble->type == "empty") {
+            print "<div style=\"height:{$bubble->height}px;\" class=\"empty\"></div>";
+          } else {
+            if($bubble->arrow) {
+              print "<div id=\"{$bubble->name}\" class=\"bubble arrow-bubble {$bubble->type}\"><p>{$bubble->name}</p></div>";
+            } else {
+              print "<div id=\"{$bubble->name}\" class=\"bubble {$bubble->type}\"><p>{$bubble->name}</p></div>";
+            }
+            if ($bubble->type != "default") {
+              print "<div class=\"name\">{$bubble->name}</div>";
+              print "<div class=\"contents\">{$bubble->text}</div>";
+            }
+            switch($bubble->arrow) {
+              case "up":
+                print "<div class=\"vertical-arrow\"><p>&uarr;</p></div>";
+                break;
+              case "down":
+                print "<div class=\"vertical-arrow\">&darr;</div>";
+                break;
+              case "updown":
+                print "<div class=\"vertical-arrow\">&#8597;</div>";
+                break;
             }
           }
         }
         print "</div>";
-      ?>
-    </div>
+      }
+    }
+    ?>
+  </div></div>
+  <div class="dropdown">
+    <?php //This php takes in an xml file, and using its information, a series of dropdowns designed for mobile displays
+      $priorType = "";
+      $first = true;
+      print "<div>";
+      $columns = simplexml_load_file('selling.xml');
+      foreach ($columns->column as $column) {
+        if ($column->title != "arrow" && $column->title != "default") {
+          foreach ($column->bubble as $bubble) {
+            if($bubble->type != "empty") {
+              if($priorType != $bubble->type) {
+                print "</div><div class=\"parent bubble {$bubble->type} column-title\"><p>{$column->title}</p></div>";
+                print "<div class=\"child\">";
+              }
+              print "<div id=\"{$bubble->name}\" class=\"bubble parent {$bubble->type}\"><p>{$bubble->name}</p></div>";
+              if ($bubble->type != "default") {
+                print "<div class=\"child contents\">{$bubble->text}</div>";
+              }
+              $priorType = (string)$bubble->type;
+            }
+          }
+        }
+      }
+      print "</div>";
+    ?>
+  </div>
 </div>
 </body>
 <script type="text/javascript">
-  <?php
-  $columns = simplexml_load_file('selling.xml');
-  //this adds functionality to each bubble such that if you click it, text changes
-  //I CAN PROBABLY USE JQUERY TO MAKE THIS NOT STUPID!!!!
-  foreach ($columns->column as $column) {
-    foreach ($column->bubble as $bubble) {
-      if ($bubble->type != "empty" && $bubble->type != "default") {
-        print "document.getElementById(\"{$bubble->name}\").addEventListener(\"click\", function(){
-          changeText(\"{$bubble->name}\", \"{$bubble->text}\");
-        });";
-      }
-    }
-  }
-  ?>
-
-  var previousBubble;
-  //TODO: change to use jquery
-  //function called above that changes the text of the information div
-  function changeText(title, description) {
-    document.getElementById("info").innerHTML = "<h1>" + title  + "</h1><p>" + description + "</p>";
-
-    document.getElementById(title).classList.add('selected');
-    if (previousBubble) {
-      document.getElementById(previousBubble).classList.remove('selected');
-    }
-    previousBubble = title;
-  }
-
-  //this functions as to create slideToggle functionality when in mobile view
   $(document).ready(function() {
-    //hide the all of the element with class contents
+    //desktop interactivity
+    $('.bubble').click(function() {
+      if ($(event.currentTarget).next().hasClass("name")) {
+        $('.bubble').removeClass('selected');
+        $(event.currentTarget).addClass('selected');
+        var title = $(event.currentTarget).next().html();
+        var description = $(event.currentTarget).next().next().html();
+        document.getElementById("info").innerHTML = "<h1>" + title  + "</h1><p>" + description + "</p>";
+      }
+    });
+    //mobile interactivity
     $(".child").hide();
     //toggle the componenet with class bubble
     $(".parent").click(function() {
       $(this).next(".child").slideToggle(600);
     });
+    $("#outer").css("zoom", $(window).width() * .00065);
+  });
+
+  $(window).resize(function() {
+    $("#outer").css("zoom", $(window).width() * .00065);
   });
 </script>
 </html>
